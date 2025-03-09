5e9ae4ea9bcb8a0ccb757fa7d1d7e51667b5300a
David Carr <david@carrcommunications.com>
2025-03-09 10:57:48 -0400

    removed logrocket

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,16 +1,7 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import LogRocket from '@logrocket/react-native';
 
 export default function RootLayout() {
-
-
-  useEffect(() => {
-    LogRocket.init('dibbvn/toastmost', {
-      updateId: Updates.isEmbeddedLaunch ? null : Updates.updateId,
-      expoChannel: Updates.channel,
-    });
-  }, []);
 
   return (
       <SafeAreaProvider>
